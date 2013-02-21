c94d1bf930f16bda90bc028502035a804565db58
Stephan Richter <stephan.richter@gmail.com>
2013-02-21 21:46:42 +0000

    Back to development: 2.0.0a2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read(*rnames):
 
 setup(
     name='zope.pluggableauth',
-    version='2.0.0a1',
+    version='2.0.0a2.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Pluggable Authentication Utility',
