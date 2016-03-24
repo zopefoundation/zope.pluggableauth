c5936b9dbf7b9071ba4f40ceebc870aee3b81d86
Tres Seaver <tseaver@palladion.com>
2016-03-24 11:06:19 -0400

    Drop support for Python 2.6.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(*rnames):
 
 setup(
     name='zope.pluggableauth',
-    version='2.0.1.dev0',
+    version='2.1.0.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Pluggable Authentication Utility',
@@ -49,7 +49,6 @@ setup(
         'License :: OSI Approved :: Zope Public License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
